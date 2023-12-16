a26083465448c45d5f7d823e57192a695c6d8f71
miyemicodes <lizzymiyemi@gmail.com>
2023-12-15 19:20:32 -0800

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const textBtn = document.getElementById("click-text");
 console.log(textBtn);
 
 textBtn.addEventListener("click", (e) =>{
-  e.target.style.background = "black";
-  e.target.style.color = "white";
+  textBtn.style.background = "black";
+  textBtn.style.color = "white";
 })
