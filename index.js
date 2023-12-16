7f8143be134b4f03e14be39034aa816cc25d0b15
miyemicodes <lizzymiyemi@gmail.com>
2023-12-15 19:28:33 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-const textBtn = document.getElementById("click-text");
+const textBtn = document.getElementById("click-text1");
+const textBtn2 = document.getElementById("click-text2");
+const textBtn3 = document.getElementById("click-text3");
+const textBtn4 = document.getElementById("click-text4");
+const textBtn5 = document.getElementById("click-text5");
+const textBtn6 = document.getElementById("click-text6");
+const textBtn7 = document.getElementById("click-text7");
+const textBtn8 = document.getElementById("click-text8");
+
 
 console.log(textBtn);
 
 textBtn.addEventListener("click", (e) =>{
   textBtn.style.background = "black";
   textBtn.style.color = "white";
-})
+});
+
+textBtn2.addEventListener("click", (e) =>{
+  textBtn2.style.background = "black";
+  textBtn2.style.color = "white";
+});
+
+textBtn3.addEventListener("click", (e) =>{
+  textBtn3.style.background = "black";
+  textBtn3.style.color = "white";
+});
+
+textBtn4.addEventListener("click", (e) =>{
+  textBtn4.style.background = "black";
+  textBtn4.style.color = "white";
+});
+
+textBtn5.addEventListener("click", (e) =>{
+  textBtn5.style.background = "black";
+  textBtn5.style.color = "white";
+});
+
+textBtn6.addEventListener("click", (e) =>{
+  textBtn6.style.background = "black";
+  textBtn6.style.color = "white";
+});
+
+textBtn7.addEventListener("click", (e) =>{
+  textBtn7.style.background = "black";
+  textBtn7.style.color = "white";
+});
+
+textBtn8.addEventListener("click", (e) =>{
+  textBtn8.style.background = "black";
+  textBtn8.style.color = "white";
+});
